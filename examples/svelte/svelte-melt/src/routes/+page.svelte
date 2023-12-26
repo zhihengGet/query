ac9048ce3e7720df878a20c4feb0e6a03db7eedb
zhihengzhanga@gmail.com <zhihengzhanga@gmail.com>
2023-12-26 00:00:25 -0700

    fix

---
+++ examples/svelte/svelte-melt/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { QueryClientProvider, QueryClient } from '@tanstack/svelte-query';
+	import { SvelteQueryDevtools } from '@tanstack/svelte-query-devtools';
+	import Simple from './test.svelte';
+
+	const queryClient = new QueryClient();
+</script>
+
+<QueryClientProvider client={queryClient}>
+	<main>
+		<Simple />
+	</main>
+	<SvelteQueryDevtools />
+</QueryClientProvider>
---
+++ examples/svelte/svelte-melt/src/routes/test.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { createQuery } from '@tanstack/svelte-query';
+
+	const data = createQuery({ queryKey: '1', queryFn: () => ['12321'] });
+</script>
+
+{JSON.stringify(data())}
