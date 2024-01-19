b25ab03a2b3fb264c89b24f98fc3073caf8ab695
zhihengzhanga@gmail.com <zhihengzhanga@gmail.com>
2024-01-19 10:28:03 -0700

    fix

---
+++ examples/svelte/svelte-melt/src/routes/test2.svelte
@@ -0,0 +1,18 @@
+<script>
+	let count = $state({});
+	let c = $state({});
+	setInterval(() => {
+		count = { b: Date.now() };
+	}, 1000);
+
+	$effect(() => {
+		c = { ...count };
+		console.log('is effect used', count);
+	});
+	function increment() {}
+</script>
+
+<button onclick={increment}>
+	clicks: {count.b}
+	{c.b}
+</button>
